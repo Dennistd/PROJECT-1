8dd6de08041f70b96949247ec036e50c0e9ed637
Dennis <xxyeezusxx@icloud.com>
2020-05-15 20:28:28 +0300

    Initial Commit

---
+++ PROJECT Calendar v3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PROJECT Calendar v3
+//
+//  Created by Dennis Dimitrov on 15.05.20.
+//  Copyright © 2020 Dennis Dimitrov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
